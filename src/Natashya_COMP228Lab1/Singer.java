77e9a50d9519aef2a0c08c1351e72c43693ce1e4
NatashyaPeddle <natashya.peddle@gmail.com>
2025-09-15 15:55:57 -0400

    Singer

---
+++ src/Natashya_COMP228Lab1/Singer.java
@@ -0,0 +1,79 @@
+package Natashya_COMP228Lab1;
+
+public class Singer {
+
+
+        //declaring variables
+        //required: id, name, address, date of birth, number of albums published
+
+        private String id;
+        private String name;
+        private String address;
+        private int birthday;
+        private int albumnumber;
+
+        //constructor - no arguements / default
+    public Singer() {
+            this.id = "default";
+            this.name = "default";
+            this.address = "default";
+            this.birthday = 0;
+            this.albumnumber = 0;
+        }
+        //constructor with 5 arguements
+    public Singer(String id, String name, String address, int birthday, int albumnumber){
+            this.id = id;
+            this.name = name;
+            this.address = address;
+            this.birthday = birthday;
+            this.albumnumber = albumnumber;
+        }
+
+        //setters
+        public void SetId (String id){
+            this.id = id;
+        }
+        public void SetName (String name){
+            this.name = name;
+        }
+        public void SetAddress (String address){
+            this.address = address;
+        }
+        public void SetBirthday ( int birthday){
+            this.birthday = birthday;
+        }
+        public void SetAlbumnumber ( int albumnumber){
+            this.albumnumber = albumnumber;
+        }
+
+        //getters
+        public String getId () {
+            return id;
+        }
+
+        public String getName () {
+            return name;
+        }
+
+        public String getAddress () {
+            return address;
+        }
+
+        public int getBirthday () {
+            return birthday;
+        }
+
+        public int getAlbumnumber () {
+            return albumnumber;
+        }
+    }
+
+
+public class SingerDriver {
+    public static void main(String[] args) { }
+}
+
+    //driver
+
+
+
